99081796169985c01fe14efe9816ab6fb7f6a107
Barak Treves <baraktreves1@gmail.com>
2022-05-23 15:49:47 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { App } from './root-cmp.jsx'
+import './assets/styles/main.scss'
+import configureStore from './js/configure-store'
+import { BrowserRouter as Router } from 'react-router-dom'
+const store = configureStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+)
--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,22 +1,16 @@
 import { AppHeader } from './js/cmps/app-header.jsx'
 import { Route, Routes } from 'react-router-dom'
 import { routes } from './routes.js'
-import { Home } from './js/pages/Home/home.jsx'
 
 export function App() {
   return (
     <div className='app'>
       {/* <AppHeader /> */}
-      <Home />
-      {/* <Routes> */}
-      {/* <Route path='/' element={<Home />} /> */}
-      {/* </Routes> */}
+      <Routes>
+        {routes.map((route, idx) => (
+          <Route key={idx} path={route.path} element={<route.component />} />
+        ))}
+      </Routes>
     </div>
   )
-}
-
-{
-  /* {routes.map((route, idx) => (
-          <Route key={idx} path={route.path} element={<route.component />} />
-        ))} */
 }
