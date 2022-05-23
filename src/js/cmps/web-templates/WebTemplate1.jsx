c892c47f3b885ff545d74eff2f507cfc83d62b7d
Amit Akuka <amit.akoka98@gmail.com>
2022-05-23 16:07:13 +0300

    added web-template-files

---
+++ src/js/cmps/web-templates/WebTemplate1.jsx
@@ -0,0 +1,7 @@
+export const WebTemplate1 = () => {
+  
+
+  return <section className="web-template1-container">
+    Hello from web-template 1
+  </section>
+}
---
+++ src/js/cmps/web-templates/WebTemplate2.jsx
@@ -0,0 +1,8 @@
+
+export const WebTemplate2 = () => {
+  
+
+  return <section className="web-template2-container">
+    Hello from web-template 2
+  </section>
+}
