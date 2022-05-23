434681088032f02caa0d24f69e2d361918793f2a
Amit Akuka <amit.akoka98@gmail.com>
2022-05-23 15:57:30 +0300

    added web-templates and web templates routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
-import { Home } from './js/pages/Home/home.jsx'
+import { Home } from './js/cmps/Home/home.jsx'
+import { WebTemplate1 } from './js/cmps/web-templates/WebTemplate1.jsx'
+import { WebTemplate2 } from './js/cmps/web-templates/WebTemplate2.jsx'
 
 export const routes = [
+  {
+    path: '/Template1',
+    component: WebTemplate1,
+  },
+  {
+    path: '/Template1',
+    component: WebTemplate2,
+  },
   {
     path: '/',
     component: Home,
