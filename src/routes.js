3919d657db3f0af622012e79e4bdb20d5edbc353
Barak Treves <baraktreves1@gmail.com>
2022-05-24 17:26:05 +0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Home } from './js/pages/Home/home.jsx'
+import { Editor } from './js/pages/Editor/editor.jsx'
 import { WebTemplate1 } from './js/cmps/web-templates/WebTemplate1.jsx'
 import { WebTemplate2 } from './js/cmps/web-templates/WebTemplate2.jsx'
 
 export const routes = [
+  {},
+  {
+    path: '/editor',
+    component: Editor,
+  },
   {
     path: '/Template1',
     component: WebTemplate1,
