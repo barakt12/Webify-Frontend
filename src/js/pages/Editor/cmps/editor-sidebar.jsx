af28f8e40ee68bd7767cc9360f640eeb28eb48c8
Barak Treves <baraktreves1@gmail.com>
2022-05-26 15:45:20 +0300

    changes to sidebar

--- src/js/pages/Editor/cmps/editor-sidebar.jsx
+++ src/js/pages/Editor/cmps/editor-sidebar.jsx
@@ -3,10 +3,10 @@ import { Draggable, Droppable } from 'react-beautiful-dnd'
 import { useEffect, useState } from 'react'
 import { templateService } from '../../../services/templates.service'
 
-export function EditorSidebar({ elements }) {
+export function EditorSidebar() {
   const [isSidebarShown, toggleSidebarShown] = useState(true)
   const [cmpList, setCmpList] = useState(null)
-  const [activeBtn, setActiveBtn] = useState('add')
+  const [activeTab, setActiveTab] = useState(null)
 
   useEffect(() => {
     const cmps = templateService.getCmpsByCategory('header')
@@ -24,7 +24,12 @@ export function EditorSidebar({ elements }) {
 
   return (
     <section className='editor-sidebar'>
-      <SidebarBtns onChooseCmps={onChooseCmps} onShowThemes={onShowThemes} />
+      <SidebarBtns
+        onChooseCmps={onChooseCmps}
+        onShowThemes={onShowThemes}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
       <Droppable droppableId='hb5' isDropDisabled={true}>
         {(provided) => (
           <div
@@ -67,7 +72,7 @@ export function EditorSidebar({ elements }) {
           width={'15'}
           height='96'
           viewBox='0 0 15 96'
-          className='sidebar-close-svg'
+          className={`sidebar-close-svg ${!isSidebarShown ? 'closed' : ''}`}
         >
           <path
             d='M0 0H3V1.00588C3.0011 4.42584 3.9102 9.97716 7.27295 13.2873C7.45088 13.4625 7.62629 13.6347 7.79957 13.8048L7.85959 13.8637C9.89318 15.8599 11.6678 17.602 12.9234 19.7206C14.0939 21.6956 14.792 23.9527 14.9602 27H15V68C15 71.7381 14.3125 74.3685 13.0144 76.6235C11.7533 78.8142 9.94312 80.5911 7.86152 82.6344L7.79905 82.6957C7.62594 82.8656 7.4507 83.0377 7.27295 83.2127C3.9102 86.5228 3.0011 92.0739 3 95.4938V96H0V0Z'
