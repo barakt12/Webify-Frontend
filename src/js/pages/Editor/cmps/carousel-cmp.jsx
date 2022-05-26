8e6ceba1ed6328e36daafc4f18e65a60975afff4
Avi Iskaov <avitech1242@gmail.com>
2022-05-26 10:07:49 +0300

    conflic-resolve

--- src/js/pages/Editor/cmps/carousel-cmp.jsx
+++ src/js/pages/Editor/cmps/carousel-cmp.jsx
@@ -1,39 +1,37 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import * as React from 'react'
+import { useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import MobileStepper from '@mui/material/MobileStepper'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
+import SwipeableViews from 'react-swipeable-views'
+import { autoPlay } from 'react-swipeable-views-utils'
 
+export const CarosuelCmp = ({ carosuelContent }) => {
+  const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
-export const CarosuelCmp = ({carosuelContent}) => {
+  console.log(carosuelContent)
+  //must get label: and imgPath
+  const images = [...carosuelContent]
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-console.log(carosuelContent)
-//must get label: and imgPath
-const images = [...carosuelContent];
-
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
+  const theme = useTheme()
+  const [activeStep, setActiveStep] = React.useState(0)
+  const maxSteps = images.length
 
   const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  }
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
 
   const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
+    setActiveStep(step)
+  }
 
   return (
     <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
@@ -46,17 +44,12 @@ const images = [...carosuelContent];
           height: 50,
           pl: 2,
           bgcolor: '#1b1b1b',
-          color: 'white'
+          color: 'white',
         }}
       >
         <Typography>{images[activeStep].label}</Typography>
       </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
+      <AutoPlaySwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
         {images.map((step, index) => (
           <div key={step.label}>
             {Math.abs(activeStep - index) <= 2 ? (
@@ -83,34 +76,21 @@ const images = [...carosuelContent];
         activeStep={activeStep}
         sx={{
           bgcolor: '#1b1b1b',
-          color: 'white'
+          color: 'white',
         }}
         nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            style={{color: 'white'}}
-            disabled={activeStep === maxSteps - 1}
-          >
+          <Button size="small" onClick={handleNext} style={{ color: 'white' }} disabled={activeStep === maxSteps - 1}>
             Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
           </Button>
         }
         backButton={
-          <Button size="small" onClick={handleBack} style={{color: 'white'}} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
+          <Button size="small" onClick={handleBack} style={{ color: 'white' }} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
             Back
           </Button>
         }
       />
     </Box>
-  );
+  )
 }
