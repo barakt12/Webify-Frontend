2df02fd8b760e0261de0db8131350d2c47f4c2ae
Avi Iskaov <avitech1242@gmail.com>
2022-05-26 15:56:34 +0300

    bug fix

--- src/js/pages/Editor/cmps/sidebar-btns.jsx
+++ src/js/pages/Editor/cmps/sidebar-btns.jsx
@@ -1,24 +1,12 @@
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Typography,
-} from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
 import React, { useState } from 'react'
-export function SidebarBtns({
-  onChooseCmps,
-  onShowThemes,
-  activeTab,
-  setActiveTab,
-}) {
+export function SidebarBtns({ onChooseCmps, onShowThemes, activeTab, setActiveTab }) {
   const [activeAddBtn, setActiveAddBtn] = useState('header')
 
   const onClickCmpBtn = async (ev, cmpName) => {
     await setActiveAddBtn(cmpName)
-    if (ev.target.previousSibling)
-      ev.target.previousSibling.classList.add('border-radius-previous')
-    if (ev.target.nextSibling)
-      ev.target.nextSibling.classList.add('border-radius-next')
+    if (ev.target.previousSibling) ev.target.previousSibling.classList.add('border-radius-previous')
+    if (ev.target.nextSibling) ev.target.nextSibling.classList.add('border-radius-next')
     onChooseCmps(cmpName)
   }
 
@@ -26,11 +14,8 @@ export function SidebarBtns({
     setActiveTab(tabName)
   }
   return (
-    <div className='editor-edit-add-btns'>
-      <button
-        className={`edit-btn ${activeTab === 'edit' ? 'active' : ''}`}
-        onClick={() => onChangeTab('edit')}
-      >
+    <div className="editor-edit-add-btns">
+      <button className={`edit-btn ${activeTab === 'edit' ? 'active' : ''}`} onClick={() => onChangeTab('edit')}>
         Edit
       </button>
       <button
@@ -42,73 +27,34 @@ export function SidebarBtns({
       >
         Themes
       </button>
-      <Accordion
-        expanded={activeTab === 'add'}
-        className='add-btn'
-        onClick={() => onChangeTab('add')}
-      >
+      <Accordion expanded={activeTab === 'add'} className="add-btn" onClick={() => onChangeTab('add')}>
         <AccordionSummary className={`add-btn-summary`}>
           <Typography>Add</Typography>
         </AccordionSummary>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'header' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'header')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'header' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'header')}>
           <Typography>Header</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'section' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'section')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'section' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'section')}>
           {' '}
           <Typography>Section</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'hero' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'hero')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'hero' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'hero')}>
           {' '}
           <Typography>Hero</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'card' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'card')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'card' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'card')}>
           {' '}
           <Typography>Card</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'text' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'text')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'text' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'text')}>
           {' '}
           <Typography>Text</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'gallery' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'gallery')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'gallery' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'gallery')}>
           {' '}
           <Typography>Gallery</Typography>
         </AccordionDetails>
-        <AccordionDetails
-          className={`add-btn-details ${
-            activeAddBtn === 'footer' ? 'active' : ''
-          }`}
-          onClick={(ev) => onClickCmpBtn(ev, 'header')}
-        >
+        <AccordionDetails className={`add-btn-details ${activeAddBtn === 'footer' ? 'active' : ''}`} onClick={(ev) => onClickCmpBtn(ev, 'footer')}>
           {' '}
           <Typography>Footer</Typography>
         </AccordionDetails>
