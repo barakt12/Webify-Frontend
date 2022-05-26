d9e3523d1e9ed5e885c68e471c546d115b6dca37
Amit Akuka <amit.akoka98@gmail.com>
2022-05-26 10:27:48 +0300

    added wap2 components to service

--- src/js/services/templates.service.js
+++ src/js/services/templates.service.js
@@ -1,14 +1,19 @@
 // Full Page Templates
 import { wapTemplate1 } from '../temaplates-example/wap-template-1'
+import { wapTemplate2 } from '../temaplates-example/wap-template-2'
 
 // Headers
 import { headerWap1 } from '../temaplates-example/headers/header-wap-1'
+import { headerWap2 } from '../temaplates-example/headers/header-wap-2'
 
 // Heros
 import { wapHero1 } from '../temaplates-example/heros/wap-hero-1'
+import { wapHero2 } from '../temaplates-example/heros/wap-hero-2'
 
 // Cards
 import { wapCards1 } from '../temaplates-example/cards/wap-cards-1'
+import { wapCards2 } from '../temaplates-example/cards/wap-cards-2'
+import { wapCards2second } from '../temaplates-example/cards/wap-cards-2-second'
 
 // Texts
 import { wapText1 } from '../temaplates-example/texts/wap-text-1'
@@ -16,6 +21,7 @@ import { wapText1 } from '../temaplates-example/texts/wap-text-1'
 // Footers
 import { wapFooter1 } from '../temaplates-example/footers/wap-footer-1'
 import { wapFooter2 } from '../temaplates-example/footers/wap-footer-2'
+import { wapFooter3 } from '../temaplates-example/footers/wap-footer-3'
 
 // Galleries
 
@@ -25,11 +31,11 @@ export const templateService = {
 }
 
 const mapCmpByCategory = {
-  header: [headerWap1],
-  hero: [wapHero1],
-  card: [wapCards1],
+  header: [headerWap1,headerWap2],
+  hero: [wapHero1,wapHero2],
+  card: [wapCards1,wapCards2,wapCards2second],
   text: [wapText1],
-  footer: [wapFooter1, wapFooter2],
+  footer: [wapFooter1, wapFooter2, wapFooter3],
 }
 
 function getCmpsByCategory(category) {
