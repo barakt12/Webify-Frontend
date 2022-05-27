da167edfda03933228d0e753c0054804a6df52d2
avi <avitech1242@gmail.com>
2022-05-27 21:43:32 +0300

    starting web-template-3

--- src/js/store/wap/wap.reducer.js
+++ src/js/store/wap/wap.reducer.js
@@ -1,31 +1,29 @@
 // import { wapService } from '../../services/wap-service'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
+import { wapTemplate3 } from "../../temaplates-example/wap-template-3";
 
 const initial_state = {
-  wap: {
-    _id: uuidv4(),
-    cmps: [],
-  },
+  wap: wapTemplate3,
   cmpsList: {
     header: [],
     hero: [],
     text: [],
   },
   selectedElement: null,
-  displaySize: '100%',
-}
+  displaySize: "100%",
+};
 
 export function wapReducer(state = initial_state, action) {
   switch (action.type) {
-    case 'SET_WAP':
-      return { ...state, wap: { ...action.wap } }
+    case "SET_WAP":
+      return { ...state, wap: { ...action.wap } };
 
-    case 'SET_ELEMENT':
-      console.log('selected element',action.cmp)
-      return { ...state, selectedElement: { ...action.cmp } }
-    case 'SET_DISPLAY_SIZE':
-      return { ...state, displaySize: action.displaySize }
+    case "SET_ELEMENT":
+      console.log("selected element", action.cmp);
+      return { ...state, selectedElement: { ...action.cmp } };
+    case "SET_DISPLAY_SIZE":
+      return { ...state, displaySize: action.displaySize };
     default:
-      return state
+      return state;
   }
 }
---
+++ src/js/temaplates-example/wap-template-3.js
@@ -0,0 +1,6 @@
+import { headerWap3 } from "./headers/header-wap-3";
+
+export const wapTemplate3 = {
+  _id: "ahlaid3",
+  cmps: [headerWap3],
+};
