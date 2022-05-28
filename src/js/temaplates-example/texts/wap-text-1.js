242ea193156a2629348290bd373d4edd78816e98
Barak Treves <baraktreves1@gmail.com>
2022-05-29 02:09:23 +0300

    minor changes in JSON

--- src/js/temaplates-example/texts/wap-text-1.js
+++ src/js/temaplates-example/texts/wap-text-1.js
@@ -17,7 +17,7 @@ export const wapText1 = {
       style: {
         fontSize: '3rem',
         fontWeight: '700',
-        paddingBottom: '1rem',
+        marginBottom: '1rem',
       },
     },
     {
@@ -30,7 +30,7 @@ export const wapText1 = {
         fontSize: '1.5rem',
         fontWeight: '700',
         color: 'rgba(249,245,242,0.5)',
-        paddingBottom: '2rem',
+        marginBottom: '2rem',
       },
     },
     {
