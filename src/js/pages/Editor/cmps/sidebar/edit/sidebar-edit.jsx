f7297aeef7e5d91676a6dc01cac644cdb0d71670
Barak Treves <baraktreves1@gmail.com>
2022-05-30 17:30:33 +0300

    made sidebar responsive

--- src/js/pages/Editor/cmps/sidebar/edit/sidebar-edit.jsx
+++ src/js/pages/Editor/cmps/sidebar/edit/sidebar-edit.jsx
@@ -7,12 +7,13 @@ import { EditColorPicker } from './edit-color-picker'
 import { ImageUrl } from './img-url-input'
 import { TxtEditor } from './txt-editor'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import SaveAltIcon from '@mui/icons-material/SaveAlt'
-
+import SaveIcon from '@mui/icons-material/Save'
 export const SidebarEdit = () => {
   const dispatch = useDispatch()
 
-  const selectedElement = useSelector((storeState) => storeState.wapModule.selectedElement)
+  const selectedElement = useSelector(
+    (storeState) => storeState.wapModule.selectedElement
+  )
 
   const onDeleteElement = () => {
     if (selectedElement) dispatch(deleteElement(selectedElement))
@@ -23,13 +24,15 @@ export const SidebarEdit = () => {
   }
 
   return (
-    <section className="editor-sidebar-container">
+    <section className='editor-sidebar-container'>
       {selectedElement && (
         <>
-          {(selectedElement.type === 'txt' || selectedElement.type === 'btn') && <TxtEditor />}
+          {(selectedElement.type === 'txt' ||
+            selectedElement.type === 'btn') && <TxtEditor />}
 
-          <div className="color-picker-container">
-            {(selectedElement.type === 'txt' || selectedElement.type === 'btn') && (
+          <div className='color-picker-container'>
+            {(selectedElement.type === 'txt' ||
+              selectedElement.type === 'btn') && (
               <>
                 <p>Font Color</p>
                 <EditColorPicker isBackgroundColor={false} />
@@ -39,22 +42,24 @@ export const SidebarEdit = () => {
             <EditColorPicker isBackgroundColor={true} />
           </div>
           {selectedElement.type === 'img' && (
-            <div className="img-url-container">
+            <div className='img-url-container'>
               <p>Image Link</p>
               <ImageUrl cmp={selectedElement} />
             </div>
           )}
         </>
       )}
-      {!selectedElement && <p className="sidebar-action-text">Please choose an element</p>}
-      <div className="action-btns">
+      {!selectedElement && (
+        <p className='sidebar-action-text'>Please choose an element</p>
+      )}
+      <div className='action-btns'>
         <button onClick={onDeleteElement}>
           {' '}
           <DeleteForeverIcon />
           <span>Delete</span>
         </button>
         <button onClick={onSaveWap}>
-          <SaveAltIcon />
+          <SaveIcon />
           <span>Save</span>
         </button>
       </div>
