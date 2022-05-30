410f31c6676630c934a3f591517a42eff2c0195a
Amit Akuka <amit.akoka98@gmail.com>
2022-05-30 21:10:29 +0300

    fixed signup

--- src/js/services/http.service.js
+++ src/js/services/http.service.js
@@ -36,7 +36,7 @@ async function ajax(endpoint, method = 'GET', data = null) {
     console.dir(err)
     if (err.response && err.response.status === 401) {
       sessionStorage.clear()
-      window.location.assign('/')
+      // window.location.assign('/')
     }
     throw err
   }
