cbff49e44a68b4f9aee084f87012cf5876b0f3bc
Barak Treves <baraktreves1@gmail.com>
2022-05-31 21:43:37 +0300

    styling home page

--- src/js/pages/Home/cmps/cards.jsx
+++ src/js/pages/Home/cmps/cards.jsx
@@ -1,5 +1,28 @@
 import React from 'react'
 
 export function Cards() {
-  return <div className='home-page-cards'>Cards</div>
+  return (
+    <div className='home-page-cards'>
+      <svg
+        class='StyledBreakerWave-pthh4u-0 XMxmK'
+        width='1140'
+        height='366'
+        viewBox='0 0 1140 366'
+        fill='none'
+      >
+        <g clip-path='url(#clip0)'>
+          <path d='M-150 179H1293V427H-150V179Z' fill='white'></path>
+          <path
+            d='M612.5 27.6721C886.118 -18.7819 1111 0.499931 1290 31.9999V450.5H-150V27.6721C-150 27.6721 171.222 102.591 612.5 27.6721Z'
+            fill='white'
+          ></path>
+        </g>
+        <defs>
+          <clipPath id='clip0'>
+            <rect width='1140' height='365.5' fill='white'></rect>
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  )
 }
---
+++ src/js/pages/Home/cmps/teaser.jsx
@@ -0,0 +1,12 @@
+export const Teaser = () => {
+  return (
+    <div className='teaser-container'>
+      <div className='img-container'>
+        <img
+          src='https://res.cloudinary.com/do0lvumuv/image/upload/v1654021420/Teaser-webify_lwiip8.jpg'
+          alt=''
+        />
+      </div>
+    </div>
+  )
+}
