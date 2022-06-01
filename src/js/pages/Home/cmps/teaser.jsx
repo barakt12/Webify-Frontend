bc987686818ab768390f872e1714afff7f5a9f7a
Barak Treves <baraktreves1@gmail.com>
2022-06-01 13:43:19 +0300

    changes to home page

--- src/js/pages/Home/cmps/teaser.jsx
+++ src/js/pages/Home/cmps/teaser.jsx
@@ -1,9 +1,9 @@
 export const Teaser = () => {
   return (
-    <div className='teaser-container'>
+    <div className='teaser-container '>
       <div className='wave-png'></div>
 
-      <div className='img-container'>
+      <div className='img-container main-layout'>
         <img
           src='https://res.cloudinary.com/do0lvumuv/image/upload/v1654021420/Teaser-webify_lwiip8.jpg'
           alt=''
