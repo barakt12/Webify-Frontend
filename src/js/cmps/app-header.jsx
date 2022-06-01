32af1b06596c885e91fd8b3fa0f7e40b484a468c
Amit Akuka <amit.akoka98@gmail.com>
2022-06-01 14:39:30 +0300

    added web hamburger and new responsive nav-menu

--- src/js/cmps/app-header.jsx
+++ src/js/cmps/app-header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
@@ -11,53 +11,61 @@ export const AppHeader = () => {
   const dispatch = useDispatch()
   const loggedUser = useSelector((storeState) => storeState.userModule.user)
 
+  const [isNavMenuShown,setisNavMenuShown] = useState(false)
+
   const onLogout = () => {
     dispatch(userLogout())
+  }
+
+  const onOpenNavMenu = () => {
+    setisNavMenuShown(!isNavMenuShown)
   }
 
   return (
     <>
       {location.pathname !== '/preview' && (
         <header>
-
-        <div className='app-header flex justify-between align-center '>
-          <Link to='/' className='clean-link logo'>
-            <p className='logo'>webify</p>
-          </Link>
-          <MenuIcon className='menu-icon' />
-          {/* <section className="main-menu-container">
-            <h1>test1</h1>
-            <h1>test2</h1>
-            <h1>test3</h1>
-          </section> */}
-          <div className='navbar flex justify-between gap-20'>
-            <Link to='/templates' className='clean-link'>
-              Templates
+          <div className={(isNavMenuShown) ? 'header-nav-menu-open app-header flex justify-between align-center' : 'app-header flex justify-between align-center'}>
+            <Link to="/" className="clean-link logo">
+              <p className="logo">webify</p>
             </Link>
-            <Link to='/editor' className='clean-link'>
-              Editor
-            </Link>
-            {!loggedUser ? (
-              <Link to='/login' className='clean-link'>
-                Login
+            <MenuIcon className="menu-icon" onClick={onOpenNavMenu}/>
+            {isNavMenuShown && <section className="main-menu-container">
+              <Link to="/templates" className="clean-link" onClick={onOpenNavMenu}>
+                Templates
               </Link>
-            ) : (
-              <>
-                <Link to='/profile' className='clean-link'>
-                  Profile
+              <Link to="/editor" className="clean-link" onClick={onOpenNavMenu}>
+                Editor
+              </Link>
+            </section>}
+            <div className="navbar flex justify-between gap-20">
+              <Link to="/templates" className="clean-link">
+                Templates
+              </Link>
+              <Link to="/editor" className="clean-link">
+                Editor
+              </Link>
+              {!loggedUser ? (
+                <Link to="/login" className="clean-link">
+                  Login
                 </Link>
-                <p>Welcome, {loggedUser.fullname.split(' ')[0]}!</p>
-                <Link
-                  to='/'
-                  onClick={onLogout}
-                  className='clean-link logout-icon'
-                >
-                  <LogoutIcon className='logout-svg' />
-                </Link>
-              </>
-            )}
+              ) : (
+                <>
+                  <Link to="/profile" className="clean-link">
+                    Profile
+                  </Link>
+                  <p>Welcome, {loggedUser.fullname.split(' ')[0]}!</p>
+                  <Link
+                    to="/"
+                    onClick={onLogout}
+                    className="clean-link logout-icon"
+                  >
+                    <LogoutIcon className="logout-svg" />
+                  </Link>
+                </>
+              )}
+            </div>
           </div>
-        </div>
         </header>
       )}
     </>
