0e861d2edd6e1d994b97980db873a78cd3383b8b
Amit Akuka <amit.akoka98@gmail.com>
2022-06-02 23:45:26 +0300

    f

--- src/js/pages/Home/cmps/teaser.jsx
+++ src/js/pages/Home/cmps/teaser.jsx
@@ -3,7 +3,7 @@ export const Teaser = () => {
     <div className='teaser-container '>
       <div className='wave-png'></div>
 
-      <div className='img-container main-layout'>
+      <div className='img-container'>
         <img
           src='https://res.cloudinary.com/do0lvumuv/image/upload/v1654021420/Teaser-webify_lwiip8.jpg'
           alt=''
